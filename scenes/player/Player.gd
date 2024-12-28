8d1383e68da28f5c39fa6c5a945cf2f86da45009
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2024-12-28 13:01:39 +0100

    Add custom keys, add export var, add body_entered

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,15 +1,28 @@
 extends RigidBody3D
 
+# Export make variable avaiable in inspector, ## make dosc
+## How much vertical force to applay when moving.
+@export_range(100,2000) var thrust: int = 1000
+@export var torque_thrust: float = 100.0
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
 	# Moving obj
-	if Input.is_action_pressed("ui_accept"):
-		apply_central_force(basis.y * delta * 1000)
+	if Input.is_action_pressed("Up"):
+		apply_central_force(basis.y * delta * thrust)
 		
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("Left"):
 		# moment obrotowty
-		apply_torque(Vector3(0.0,0.0,100.0 * delta))
+		apply_torque(Vector3(0.0,0.0,torque_thrust * delta))
 		
-	if Input.is_action_pressed("ui_right"):
-		apply_torque(Vector3(0.0,0.0,100.0 * (-delta)))
+	if Input.is_action_pressed("Right"):
+		apply_torque(Vector3(0.0,0.0,torque_thrust * (-delta)))
+
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Win"):
+		print("You win bro!")
+		
+	elif body.is_in_group("Lose"):
+		print("Try again!")
