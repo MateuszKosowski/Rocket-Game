27bb2f91d5fe9909c6f5f7f8655b6f48f1ba33b7
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2024-12-28 12:24:04 +0100

    Apply force, torque and basis_y

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,12 +1,15 @@
-extends Node3D
+extends RigidBody3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
 	# Moving obj
 	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
+		apply_central_force(basis.y * delta * 1000)
+		
 	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
+		# moment obrotowty
+		apply_torque(Vector3(0.0,0.0,100.0 * delta))
+		
 	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)	
+		apply_torque(Vector3(0.0,0.0,100.0 * (-delta)))
