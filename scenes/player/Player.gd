2504d988b6235ce91af14d49ac61416dbc6eff9b
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2025-02-07 23:26:26 +0100

    Add dash

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -7,14 +7,21 @@ extends RigidBody3D
 
 var is_transitioning: bool = false
 
+@export var dash_force: float = 12.0
+@export var dash_duration: float = 0.5
+@export var dash_cooldown: float = 5.0
+var is_dash_avaiable: bool = true 
+
 @onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
 @onready var win_audio: AudioStreamPlayer = $WinAudio
 @onready var fly_audio: AudioStreamPlayer3D = $FlyAudio
+@onready var dash_audio: AudioStreamPlayer3D = $DashAudio
 @onready var engine_particles: GPUParticles3D = $MainBooster
 @onready var right_engine_particles: GPUParticles3D = $RightBooster
 @onready var left_engine_particles: GPUParticles3D = $LeftBooster
 @onready var explosion_particles: GPUParticles3D = $ExplosionParticles
 @onready var success_particles: GPUParticles3D = $SuccessParticles
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -29,6 +36,9 @@ func _process(delta: float) -> void:
 		fly_audio.stop()
 		engine_particles.emitting = false
 		
+	if Input.is_action_just_pressed("Dash") && is_dash_avaiable == true:
+		start_dash()
+		
 	if Input.is_action_pressed("Left"):
 		# moment obrotowty
 		apply_torque(Vector3(0.0,0.0,torque_thrust * delta))
@@ -41,7 +51,27 @@ func _process(delta: float) -> void:
 		left_engine_particles.emitting = true
 	else:
 		left_engine_particles.emitting = false
+	
+		
+func start_dash() -> void:
+	is_dash_avaiable = false
+	
+	#Make dash
+	apply_central_impulse(basis.y * dash_force)
+	dash_audio.play()
+	engine_particles.emitting = true
+	
+	#Wait 
+	await get_tree().create_timer(dash_duration).timeout
+	
+	dash_audio.stop()
+	engine_particles.emitting = false
+	
+	#Dash cooldown
+	await get_tree().create_timer(dash_cooldown).timeout
+	is_dash_avaiable = true
 
+	
 func turn_off_booster() -> void:
 	engine_particles.emitting = false
 	left_engine_particles.emitting = false
